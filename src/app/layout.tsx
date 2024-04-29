7f9ff17e9fcdf3b7de334a6964852caebc7aa588
Pierre LEVEQUE <pierreleveque.pro@gmail.com>
2024-04-29 12:52:24 +0200

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "../styles/index.scss";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   weight: ["300", "400", "500", "600", "700"],
@@ -25,6 +26,7 @@ export default function RootLayout({
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       ></meta>
+      <Analytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
